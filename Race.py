3406cdeabef212994553bced95e75cc1920e22d2
Moonseon <moonseonyoon@gmail.com>
2025-03-13 21:18:20 +0900

    changed name of the window

--- Race.py
+++ Race.py
@@ -1,80 +1,78 @@
-from graphics import *
+from graphics import*
 from Dice import Dice
 
 class Horse:
-    def __init__(self, speed, y_pos, image_file, window):
-        self.speed = speed
-        self.dice = Dice(speed)
-        self.x_pos = 0
-        self.y_pos = y_pos
-        self.window = window
+   def __init__(self, speed, y_pos, image, window):
+       self.speed = speed
+       self.dice = Dice(speed)
+       self.x_pos = 0
+       self.y_pos = y_pos
+       self.image = image
+       self.window = window
+       #self.image.draw(self.window)
 
-        # Create Image inside the class instead of passing it
-        self.image = Image(Point(self.x_pos, self.y_pos), image_file)
+   def move(self):
+       roll = self.dice.roll()
+       self.x_pos += roll
+       #self.image.move(roll, 0)
 
-    def move(self):
-        roll = self.dice.roll()
-        self.x_pos += roll
-        self.image.move(roll, 0)  # Move image along the x-axis
 
-    def draw(self):
-        self.image.draw(self.window)
+   def draw(self):
+       self.image.draw_at_pos(self.window, self.x_pos, self.y_pos)
 
-    def crossed_finish_line(self, finish_line_x):
-        return self.x_pos >= finish_line_x
+
+   def crossed_finish_line(self, finish_line_x):
+       return self.x_pos >= finish_line_x
+
 
 def main():
-    # Create window
-    win = GraphWin("Race Field", 700, 350, autoflush=False)
-    win.setBackground(color_rgb(0, 255, 0))
+   #Creating window
+   win = GraphWin("Race field", 700, 350, autoflush= False)
+   win.setBackground(color_rgb(0, 255, 0))
 
-    # Add start message text
-    start_text = Text(Point(350, 20), "Click to start the race!")
-    start_text.setSize(14)
-    start_text.setStyle("bold")
-    start_text.draw(win)
+   #Set horse image
+   horse1_image = "Knight.gif"
+   horse2_image = "Wizard.gif"
 
-    # Create horses (pass filenames instead of Image objects)
-    horse1 = Horse(6, 100, "Knight.gif", win)
-    horse2 = Horse(6, 150, "Wizard.gif", win)
+   horse1_image = Image(Point(0,100), "Knight.gif")
+   horse2_image = Image(Point(0, 150), "Wizard.gif")
 
-    # Draw horses initially
-    horse1.draw()
-    horse2.draw()
+   #Set horse1, horse2
+   horse1 = Horse(6, 50, horse1_image, win)
+   horse2 = Horse(6, 150, horse2_image, win)
 
-    # Draw finish line
-    finish_line = Line(Point(650, 0), Point(650, 350))
-    finish_line.setWidth(2)
-    finish_line.draw(win)
+   #Draw finish line
+   finish_line = Line(Point(650,0), Point(650, 350))
+   finish_line.draw(win)
+   win.getMouse()
 
-    # Wait for mouse click to start race
-    win.getMouse()
-    start_text.undraw()  # Remove start message before the race begins
+   race_over = False
+   while not race_over:
+       win.clear_win()
 
-    race_over = False
-    while not race_over:
-        # Move horses
-        horse1.move()
-        horse2.move()
+       #Move horse
+       horse1.move()
+       horse2.move()
+       horse1.draw()
+       horse2.draw()
+       finish_line.draw(win)
+       update(10)
 
-        # Check for winner
-        if horse1.crossed_finish_line(650) and horse2.crossed_finish_line(650):
-            race_over = True
-            print("Tie")
-        elif horse1.crossed_finish_line(650):
-            race_over = True
-            print("Horse 1 is the winner")
-        elif horse2.crossed_finish_line(650):
-            race_over = True
-            print("Horse 2 is the winner")
-
-        update(10)
-
-    # Wait for final mouse click before closing
-    win.getMouse()
-    win.close()
-
+       if horse1.crossed_finish_line(650) and horse2.crossed_finish_line(650):
+           race_over = True
+           print("Tie")
+       elif horse1.crossed_finish_line(650):
+           race_over = True
+           print("The Crab is the winner")
+       elif horse2.crossed_finish_line(650):
+           race_over = True
+           print("The Snake is the winner")
+   win.getMouse()
+   win.close()
 if __name__ == "__main__":
-    main()
+
+   main()
+
+
 
 
